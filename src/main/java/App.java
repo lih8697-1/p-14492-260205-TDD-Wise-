14479a6405957fb8d30b89a1c56c69a5f5672522
lih8697-1 <lih8697@gmail.com>
2026-02-06 12:26:43 +0900

    12 - g

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
 
     private Scanner sc;
+    int lastId = 0;
 
     public App(Scanner sc) {
         this.sc  = sc;
@@ -12,19 +13,24 @@ public class App {
 
 
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) ");
-        String cmd = sc.nextLine();
 
-        switch (cmd) {
-            case "등록" -> {
-                System.out.print("명언 : ");
-                String saying = sc.nextLine();
-                System.out.print("작가 : ");
-                String author = sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
-            }
-            case "종료" -> {
-                return;
+        while (true){
+            System.out.println("명령) ");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String saying = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+
+                    lastId++;
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "종료" -> {
+                    return;
+                }
             }
         }
     }
