7cf5e038e01495a0c652e46fb2bec82573389d68
lih8697-1 <lih8697@gmail.com>
2026-02-06 11:45:52 +0900

    9 - r

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,11 +8,9 @@ public class AppTest {
     @Test
     @DisplayName("'== 명언 앱 ==' 출력")
     void t1() throws Exception {
-        String out = AppTestRunner.run(
-                """
+        String out = AppTestRunner.run("""
                         종료
-                        """
-        );
+                        """);
 
         assertThat(out).contains("== 명언 앱 ==");
 
@@ -21,16 +19,16 @@ public class AppTest {
     @Test
     @DisplayName("'등록")
     void t2() throws Exception {
-        String out = AppTestRunner.run(
-                """
+        String out = AppTestRunner.run("""
                         등록
                         현재를 사랑하라.
                         작자미상
                         종료
-                        """
-        );
+                        """);
 
-        assertThat(out).contains("== 명언 앱 ==");
+        assertThat(out).contains("명령) ");
+        assertThat(out).contains("명언 : ");
+        assertThat(out).contains("작가 : ");
 
     }
 }
