ca68aaabc100900c1de0d2f160dbcd186104f89d
lih8697-1 <lih8697@gmail.com>
2026-02-06 11:33:25 +0900

    7 - g

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -4,15 +4,8 @@ import java.util.Scanner;
 
 public class TestUtil {
 
-//    public static String run(String input) {
-//
-//        // 입력값
-//        Scanner scan = new Scanner(input);
-//
-//        // 명언 프로그램 실행
-//
-//        // 출력값
-//    }
+    private static PrintStream ORIGINAL_OUT = System.out;
+    private static PrintStream CURRENT_OUT = System.out;
 
     public static Scanner genScanner(String input) {
         return new Scanner(input);
@@ -24,7 +17,14 @@ public class TestUtil {
         PrintStream printStream = new PrintStream(byteArrayOutputStream);
 
         System.setOut(printStream);
+        CURRENT_OUT = printStream;
 
         return byteArrayOutputStream;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream outputStream) throws Exception {
+        System.setOut(ORIGINAL_OUT);
+        outputStream.close();
+        CURRENT_OUT.close();
     }
 }
