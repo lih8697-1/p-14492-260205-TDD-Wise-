607b6764ddb6fbc81193486c6bebc36ba2c61b7e
lih8697-1 <lih8697@gmail.com>
2026-02-06 12:17:53 +0900

    2단계 Blue

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,9 +8,7 @@ public class AppTest {
     @Test
     @DisplayName("'== 명언 앱 ==' 출력")
     void t1() throws Exception {
-        String out = AppTestRunner.run("""
-                        종료
-                        """);
+        String out = AppTestRunner.run("");
 
         assertThat(out).contains("== 명언 앱 ==");
 
@@ -23,7 +21,6 @@ public class AppTest {
                         등록
                         현재를 사랑하라.
                         작자미상
-                        종료
                         """);
 
         assertThat(out).contains("명령) ");
