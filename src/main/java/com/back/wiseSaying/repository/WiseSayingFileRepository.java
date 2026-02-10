9a233fed53958321595f67f832e910fe4bb21e07
lih8697-1 <lih8697@gmail.com>
2026-02-10 14:27:14 +0900

    49

--- src/main/java/com/back/wiseSaying/repository/WiseSayingFileRepository.java
+++ src/main/java/com/back/wiseSaying/repository/WiseSayingFileRepository.java
@@ -1,8 +1,11 @@
 package com.back.wiseSaying.repository;
 
 import com.back.standard.util.Util;
+import com.back.wiseSaying.dto.PageDto;
 import com.back.wiseSaying.entity.WiseSaying;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -20,9 +23,17 @@ public class WiseSayingFileRepository {
             String jsonStr = Util.json.toString(wiseSayingMap);
             Util.file.set("%s/%d.json".formatted(getDbPath(), wiseSaying.getId()), jsonStr);
 
+            return wiseSaying;
         }
 
+        String jsonStr = Util.json.toString(wiseSaying.toMap());
+        Util.file.set("%s/%d.json".formatted(getDbPath(), wiseSaying.getId()), jsonStr);
+
         return wiseSaying;
+    }
+
+    public void delete(WiseSaying wiseSaying1) {
+        Util.file.delete("%s/%d.json".formatted(getDbPath(), wiseSaying1.getId()));
     }
 
     private int getLastId() {
@@ -52,5 +63,33 @@ public class WiseSayingFileRepository {
 
     public String getDbPath() {
         return "db/wiseSaying";
+    }
+
+    public List<WiseSaying> findAll() {
+        return Util.file.walkRegularFiles(getDbPath(), "^\\d+\\.json$")
+                .map(path -> Util.file.get(path.toString(), ""))
+                .map(Util.json::toMap)
+                .map(WiseSaying::fromMap)
+                .toList();
+    }
+
+    public PageDto findByContentContainingDesc(String kw, int pageSize, int pageNo) {
+        List<WiseSaying> filteredWiseSayings = findAll().stream()
+                .filter(wiseSaying -> wiseSaying.getSaying().contains(kw))
+                .sorted(Comparator.comparing(WiseSaying::getId).reversed())
+                .toList();
+
+        return pageOf(filteredWiseSayings, pageNo, pageSize);
+    }
+
+    private PageDto pageOf(List<WiseSaying> filteredContent, int pageNo, int pageSize) {
+
+        List<WiseSaying> content = filteredContent.stream()
+                .skip((pageNo-1) * pageSize)
+                .limit(pageSize)
+                .toList();
+
+        int totalItems = filteredContent.size();
+        return new PageDto(pageNo, pageSize, totalItems, content);
     }
 }
